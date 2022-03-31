dbae09ea1706586ac5377c759b2e9136b375504b
Lapis0875 <lapis0875@kakao.com>
2022-03-31 15:45:21 +0900

    feat: Add onEnable / onDisable info log.

--- src/main/java/tech/lapis0875/randomsurvival/RandomSurvival.java
+++ src/main/java/tech/lapis0875/randomsurvival/RandomSurvival.java
@@ -1,5 +1,6 @@
 package tech.lapis0875.randomsurvival;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 import tech.lapis0875.randomsurvival.commands.RSCommandExecutor;
@@ -11,12 +12,15 @@ public final class RandomSurvival extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        Bukkit.getLogger().info("RandomSurvival is loaded. Registering CommandExecutor...");
         CommandExecutor commandExecutor = new RSCommandExecutor();
         Objects.requireNonNull(getCommand(Constants.Commands.RS)).setExecutor(commandExecutor);
+        Bukkit.getLogger().info("Done! Successfully enabled RS!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        Bukkit.getLogger().info("RandomSurvival is unloaded.");
     }
 }
