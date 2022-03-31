4d29922ea13ec7653965f9f33aa00709d239739f
Lapis0875 <lapis0875@kakao.com>
2022-03-31 15:47:42 +0900

    feat: Update SwapLocationGame
    - Add getter, setter for attribute 'delay'
    - Add info message in start, stop method.

--- src/main/java/tech/lapis0875/randomsurvival/games/swap_location/SwapLocationGame.java
+++ src/main/java/tech/lapis0875/randomsurvival/games/swap_location/SwapLocationGame.java
@@ -1,21 +1,40 @@
 package tech.lapis0875.randomsurvival.games.swap_location;
 
+import net.kyori.adventure.text.Component;
 import tech.lapis0875.randomsurvival.Constants;
 import tech.lapis0875.randomsurvival.games.RSGame;
 
 public class SwapLocationGame extends RSGame {
     private int delay;  // delay as seconds.
-    public SwapLocationGame(int delay) {
+
+    public SwapLocationGame(int delay) throws IllegalStateException{
         super();
         this.delay = delay;
     }
+
     public SwapLocationGame() {
         this(Constants.DEFAULT_DELAY);
     }
+
+    public void setDelay(int newDelay) {
+        this.delay = newDelay;
+    }
+
+    public int getDelay() {
+        return this.delay;
+    }
+
     @Override
     public void start() {
+        this.plugin.getServer().sendMessage(Component.text("SwapLocation 게임을 시작합니다.", Constants.COLOR_GREEN));
         this.collectPlayers();
         this.scheduledTask = new SwapLocationRunnable(this.plugin, this.players);
         this.scheduledTask.runTaskTimer(this.plugin, 0, 20L * this.delay);
+    }
+
+    @Override
+    public void stop() {
+        this.plugin.getServer().sendMessage(Component.text("SwapLocation 게임을 종료합니다.", Constants.COLOR_GREEN));
+        super.stop();
     }
 }
